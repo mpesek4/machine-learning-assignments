0efb72919008e57001c56a4898ba188748e183ef
Mike <pesekm@dupage.edu>
2016-12-19 15:51:19 -0600

    apply new short technique for iterating over all moves, remove old technique on knight

--- chess_recursion.py
+++ chess_recursion.py
@@ -14,60 +14,37 @@ def find_legal_moves(gamestate,i,j,player):
         opposing_piece == 'w'
     else:
         opposing_piece == 'b'
-    if gamestate[i][j][1] == 'N':
-        # possible moves: up up left, up up right, right right up, right right down, down down left, down down right, left left           #up, left left right
-        if i+2 <=3 and j-1 >=0 and gamestate[i+2][j-1][0] in [opposing_piece,'0'] : # if all checks pass it is a legal move
-            # now we must create a deep copy of gamestate, update it with the legal move and add it to list we will return
-            # which holds all the legal game states for the next move
-            new_gamestate = copy.deepcopy(gamestate)
-            new_gamestate[i][j] = ('0',0) # legal move so original space becomes empty
-            new_gamestate[i+2][j-1] = ('N', player) # knight has moved to legal spot
-            states.append(new_gamestate)
-        if i+2 <=3 and j+1 <=3 and gamestate[i+2][j+1][0] in [opposing_piece,'0']:
-            new_gamestate = copy.deepcopy(gamestate)
-            new_gamestate[i][j] = ('0',0) # legal move so original space becomes empty
-            new_gamestate[i+2][j+1] = ('N', player) # knight has moved to legal spot
-            states.append(new_gamestate)
-        if i+1 <=3 and j+2 <=3 and gamestate[i+1][j+2][0] in [opposing_piece,'0']:
-            new_gamestate = copy.deepcopy(gamestate)
-            new_gamestate[i][j] = ('0',0) # legal move so original space becomes empty
-            new_gamestate[i+1][j+2] = ('N', player) # knight has moved to legal spot
-            states.append(new_gamestate)
-        if i+1 <=3 and j-2 >=0 and gamestate[i+1][j-2][0] in [opposing_piece,'0']:
-            new_gamestate = copy.deepcopy(gamestate)
-            new_gamestate[i][j] = ('0',0) # legal move so original space becomes empty
-            new_gamestate[i+1][j-2] = ('N', player) # knight has moved to legal spot
-            states.append(new_gamestate)
-        if i-2 >=0 and j-1 >=0 and gamestate[i-2][j-1][0] in [opposing_piece,'0']:
-            new_gamestate = copy.deepcopy(gamestate)
-            new_gamestate[i][j] = ('0',0) # legal move so original space becomes empty
-            new_gamestate[i-22][j-1] = ('N', player) # knight has moved to legal spot
-            states.append(new_gamestate)
-        if i-2 >=0 and j+1 <=3 and gamestate[i-2][j+1][0] in [opposing_piece,'0']:
-            new_gamestate = copy.deepcopy(gamestate)
-            new_gamestate[i][j] = ('0',0) # legal move so original space becomes empty
-            new_gamestate[i-2][j+1] = ('N', player) # knight has moved to legal spot
-            states.append(new_gamestate)
-        if i-1 >=0 and j+2 <=3 and gamestate[i-1][j+2][0] in [opposing_piece,'0']:
-            new_gamestate = copy.deepcopy(gamestate)
-            new_gamestate[i][j] = ('0',0) # legal move so original space becomes empty
-            new_gamestate[i-1][j+2] = ('N', player) # knight has moved to legal spot
-            states.append(new_gamestate)
-        if i-1 >=0 and j-2 >=0 and gamestate[i-1][j-2][0] in [opposing_piece,'0']:
-            new_gamestate = copy.deepcopy(gamestate)
-            new_gamestate[i][j] = ('0',0) # legal move so original space becomes empty
-            new_gamestate[i-1][j-2] = ('N', player) # knight has moved to legal spot
-            states.append(new_gamestate)
-            
-        # all moves are done if piece was a knight, other pieces have more moves, lets think of a more clever way to check all 
-        # moves
-        bishop_moves = [(1,1),(2,2),(3,3),(-1,-1),(-2,-2),(-3,-3),(1,-1),(2,-2),(3,-3),(-1,1),(-2,2),(-3,3)]
-        rook_moves = [(1,0),(2,0),(3,0),(-1,0),(-2,0),(-3,0),(0,1),(0,2),(0,3),(0,-1),(0,-2),(0,-3)]
-        # queen moves are a concatenation of the above 2 lists
-        if gamestate[i][j][1] == 'B':
-            for move in bishop_moves:
-                if (i+move[0] <=3 and >=0) and (j+move[1] <=3 and >=0):
-                    new_gamestate = copy.deepcopy(gamestate)
+    for i in range(0,4):
+        for j in range(0,4):
+            knight_moves = [(2,1),(2,-1),(1,2),(1,-2),(-2,1),(-2,-1),(-1,2),(-1,-2)]
+            bishop_moves = [(1,1),(2,2),(3,3),(-1,-1),(-2,-2),(-3,-3),(1,-1),(2,-2),(3,-3),(-1,1),(-2,2),(-3,3)]
+            rook_moves = [(1,0),(2,0),(3,0),(-1,0),(-2,0),(-3,0),(0,1),(0,2),(0,3),(0,-1),(0,-2),(0,-3)]
+            # queen moves are a concatenation of the above 2 lists
+            if gamestate[i][j][1] == 'N':
+                for move in knight_moves:
+                    if (i+move[0] <=3 and >=0) and (j+move[1] <=3 and >=0) and gamestate[i+move[0]][j+move[1]][0] in \                                  [opposing_piece,'0']:
+                        new_gamestate = copy.deepcopy(gamestate)
+                        new_gamestate[i][j] = ('0',0)
+                        new_gamestate[i+move[0]][j+move[1]] = ('B',player)
+            if gamestate[i][j][1] == 'B':
+                for move in bishop_moves:
+                    if (i+move[0] <=3 and >=0) and (j+move[1] <=3 and >=0) and gamestate[i+move[0]][j+move[1]][0] in \                                  [opposing_piece,'0']:
+                        new_gamestate = copy.deepcopy(gamestate)
+                        new_gamestate[i][j] = ('0',0)
+                        new_gamestate[i+move[0]][j+move[1]] = ('B',player)
+
+            if gamestate[i][j][1] == 'R':
+                for move in rook_moves:
+                    if (i+move[0] <=3 and >=0) and (j+move[1] <=3 and >=0) and gamestate[i+move[0]][j+move[1]][0] in \                                  [opposing_piece,'0']:
+                        new_gamestate = copy.deepcopy(gamestate)
+                        new_gamestate[i][j] = ('0',0)
+                        new_gamestate[i+move[0]][j+move[1]] = ('B',player) 
+            if gamestate[i][j][1] == 'Q':
+                for move in bishop_moves+rook_moves:
+                    if (i+move[0] <=3 and >=0) and (j+move[1] <=3 and >=0) and gamestate[i+move[0]][j+move[1]][0] in \                                  [opposing_piece,'0']:
+                        new_gamestate = copy.deepcopy(gamestate)
+                        new_gamestate[i][j] = ('0',0)
+                        new_gamestate[i+move[0]][j+move[1]] = ('B',player)
 def win_in_one(gamestate,moves_left,player):
     # if it is blacks move, white cannot win in one move, so call the function again on iterations and decrease moves_left
     if player == 'black':
