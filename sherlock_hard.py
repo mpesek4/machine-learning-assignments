e71ace34f456acf30c5ea5a6229f8e0f91952e57
Mike <pesekm@dupage.edu>
2017-02-16 17:29:27 -0600

    passes all test cases, edge cases in different potential ranges tested, commented

--- sherlock_hard.py
+++ sherlock_hard.py
@@ -1,3 +1,7 @@
+''' Problem description: given array a1....an and range of (M,P) find the value 
+within range that maximizes the function argmin{|aj-M|, 1<= i <= n}
+if multiple solutions print the smallest one'''
+
 import sys
 import math
 n = raw_input().strip()
@@ -11,39 +15,51 @@ q= p_q[1]
 
 max_gap =0
 max_gap_index = 0
+out_of_range = False
 for i in range (0,len(arr)-1):
-    if arr[i] < p:
-        continue
+    # general plan is to find the biggest gap in our sorted array because the middle value of that gap
+    # is the correct value of our function barring some edge cases
     if arr[i] > q:
         break
+    if arr[i+1] < p:
+        continue # these 2 achecks above make sure we are within our range
     if arr[i+1]-arr[i] > max_gap:
         if (arr[i+1]+arr[i]) / 2 > q:
             gap_check = q-arr[i]
             if gap_check > max_gap:
                 max_gap = gap_check
                 max_gap_index= i
+                out_of_range = True
+                optimal = q
         else:
             max_gap = arr[i+1]-arr[i]
             max_gap_index = i
+if out_of_range == False:
+    optimal = (arr[max_gap_index]+arr[max_gap_index+1]) / 2
 
-optimal = (arr[max_gap_index]+arr[max_gap_index+1]) / 2
-
-# need to check if a number outside of range can get same result
+# need to check if a number inside of our range but outside of the array is better, 2 simple checks
 
 maximized_min_diff = optimal - arr[max_gap_index]
 
-additional_check_left = arr[0]-maximized_min_diff
-additional_check_right = arr[0]+maximized_min_diff
+
 start = p
-index = 0
-while p_q[index] < arr[0]:
-    if abs(p_q[index]- arr[0]) >= maximized_min_diff:
-        optimal=p_q[index]
+index = p_q[0]
+found = False
+while index < arr[0]:
+    # first check is making sure all values between P and arr[0] maximize our function
+    if abs(index- arr[0]) >= maximized_min_diff:
+        optimal=index
+        maximized_min_diff = abs(index- arr[0])
         break
     index+=1
 index = p_q[-1]
-while index > arr[-1] and index> 0:
-    if p_q[index]- arr[-1] >= maximized_min_diff:
-        optimal = p_q[index]
+
+while index > arr[-1] and index>= p_q[0]:
+    # second check is to make sure all numbers between arr[-1] and Q maximize our function
+    if index - arr[-1] > maximized_min_diff:
+        optimal = index
+        break
     index-=1
 print optimal
+
+    
